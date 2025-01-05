0bd2d19297c05e77c27fc9c7f2359fa44fcb036f
alexabrinuss <alexsandrinus17@gmail.com>
2025-01-05 16:05:46 +0400

    second commit

---
+++ LEVEL.16/Homework/hw.py
@@ -0,0 +1,6 @@
+#while loop ის გამოყენებით დაბეჭდეთ ყველა რიცხვი 1 დან 10 მდე
+num = 1
+
+while num < 11:
+    print(num)
+    num += 1
---
+++ LEVEL.16/Homework/hw00.py
@@ -0,0 +1,9 @@
+#while loop ის გამოყენებით დაწერეთ პაროლის შემოწმების კოდი, მომხმარებეს შემოატანინეთ პაროლი და შემდეგ იქამდე ჰკითხეთ შეიყვანოს მეორედ სანამ იგივეს არ შეიყვას
+num= input("Please enter your password: ")
+
+
+while num != "madona Chimakadze": 
+    print("Password is incorrect.")
+    num= input("Please enter your password again: ") 
+
+print("Password is correct")
